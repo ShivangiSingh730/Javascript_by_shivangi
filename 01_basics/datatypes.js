f9f3ea7cd6f971aca72a5e03fcddc9c1afdd3a7f
Shivangi  Singh <pv8893148@gmail.com>
2025-12-18 05:25:13 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JScode as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); console.log("hitesh") // code readibility should be high
+
+let name="shivangi"
+let age=18
+let isLoggedtIn=false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // output  is object b'coz null is an object in JS
+console.log(typeof undefined) // output is undefined
+
