21984e21a07c9a2c572ff510092838233c0f7e42
Shivangi  Singh <pv8893148@gmail.com>
2025-12-21 06:43:17 +0000

    comparsion

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0); //comparsion operator >= convert null into number 0
+//that's why its true
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0); // always give false
+
+//===
+//strictly checking equality
+
+console.log("2"===2);
+
+
+
+
+
+
